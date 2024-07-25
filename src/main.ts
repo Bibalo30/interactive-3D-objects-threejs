c9e31d8a39e82b3a6e082b56ac0711949f5555be
Bibalo30 <pablogr30@hotmail.com>
2024-07-25 11:20:16 -0600

    added third geometry and functionality for buttons

--- src/main.ts
+++ src/main.ts
@@ -18,40 +18,44 @@ window.addEventListener('resize', () => {
 const radius = .3;
 let widthSegments = 1;
 let heightSegments = 1;
+/*const torRadius = 5;
+const torTubeRadius = 1;
+const torRaidialSegments = 16;
+const torTubularSegments = 12;*/
 
 const sphereGeometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
 const geometry = new THREE.BoxGeometry()
-//const thorusGeometry = new THREE.TorusGeometry()
+const torusGeometry = new THREE.TorusGeometry()
 
 const material = new THREE.MeshNormalMaterial({ wireframe: false })
 const material2 = new THREE.MeshNormalMaterial({ wireframe: true })
-
-
-/*const torRadius;
-const torTubeRadius;
-const torRaidialSegments;
-const torTubularSegments; */
+let useMaterial1 = true;
 
 
 let cubeScale = .1;
 //cubescaleFinalValue = .35
 const sphere = new THREE.Mesh(sphereGeometry, material2);
-const cube = new THREE.Mesh(geometry, material)
-
+const cube = new THREE.Mesh(geometry, material);
+const torus = new THREE.Mesh(torusGeometry, material);
 
 const btnSphere = document.querySelector('.btnSphere');
 const btnSphereReset = document.querySelector('.btnSphereReset');
 const btnCube = document.querySelector('.btnCube');
 const btnCubeReset = document.querySelector('.btnCubeReset');
+const btnTorus = document.querySelector('.btnTorus');
+const btnTorusReset = document.querySelector('.btnTorusReset');
 
 let resetSphere = false;
 let resetCube = false;
 
 cube.scale.set(cubeScale, cubeScale, cubeScale);
+torus.scale.set(.2, .2, .2);
 cube.position.set(0, .1, 0);
 sphere.position.set(-1.3, .1, 0)
+torus.position.set(1.3, .1, 0)
 scene.add(cube)
 scene.add(sphere);
+scene.add(torus);
 
 btnSphere.addEventListener('click', function () {
   if (resetSphere === false) {
@@ -189,6 +193,30 @@ btnCubeReset.addEventListener('click', function () {
 
   scaleCubeReset();
 });
+btnTorus.addEventListener('click', function () {
+  // Toggle between the two materials
+  //if (useMaterial1) {
+  torus.material = material2; // Assign material2 to the torus
+  //}
+
+  //useMaterial1 = !useMaterial1; // Toggle the flag for the next click
+
+  // You may need to re-render the scene if necessary
+  // renderer.render(scene, camera);
+});
+
+btnTorusReset.addEventListener('click', function () {
+  // Toggle between the two materials
+  //if (!useMaterial1) {
+  torus.material = material; // Assign material2 to the torus
+  //}
+
+  //useMaterial1 = useMaterial1; // Toggle the flag for the next click
+
+  // You may need to re-render the scene if necessary
+  // renderer.render(scene, camera);
+});
+
 
 function animate() {
   requestAnimationFrame(animate)// Request the browser to call animate() on the next frame
@@ -204,6 +232,8 @@ function animate() {
   cube.rotation.y += 0.01
   sphere.rotation.x += 0.01
   sphere.rotation.y += 0.01
+  torus.rotation.x += 0.01
+  torus.rotation.y += 0.01
 
 
 
